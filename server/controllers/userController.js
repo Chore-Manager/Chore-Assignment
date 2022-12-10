ebaf4119e4ab5f710edb01acf078d62f498160a8
John Donovan <jodonovan845@gmail.com>
2022-12-10 15:25:02 -0500

    this will add in the createUser and removeUser functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,23 +4,37 @@ const db = require('../model/dbModel');
 const userController = {};
 
 userController.getUsers = async (req, res, next) => {
-  const sqlQuery = 'SELECT * FROM user';
-  let response = await db.query(sqlQuery);
-  response = await response.json();
-  console.log('this is the response after response.json: ', response);
-  res.locals.users = response;
+  const sqlQuery = 'SELECT * FROM users';
+  const result = await db.query(sqlQuery);
+  console.log('this is the response after response.json: ', result.rows);
+  res.locals.users = result.rows;
+  return next();
 };
 
 userController.createUser = async (req, res, next) => {
   const { name } = req.body;
+  res.locals.newUser = name;
   const sqlQuery = `
     INSERT INTO users (name)
     VALUES ($1)
     `;
   const insertArray = [name];
-  db.query(sqlQuery, insertArray);
+  const result = await db.query(sqlQuery, insertArray);
+  console.log('this is the response: ', result);
+  return next();
 };
 
-userController.removeUser = async (req, res, next) => {};
+userController.removeUser = async (req, res, next) => {
+  const { name } = req.body;
+  console.log(name);
+  res.locals.removedUser = name;
+  const sqlQuery = `
+  DELETE FROM users
+  WHERE name=$1
+  `;
+  const insertArray = [name];
+  const result = await db.query(sqlQuery, insertArray);
+  return next();
+};
 
 module.exports = userController;
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -8,18 +8,18 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 // get user - populates user dropdown
-router.get('/user', userController.getUsers, (req, res) => {
+router.get('/', userController.getUsers, (req, res) => {
   return res.status(200).json(res.locals.users);
 });
 
 // add user - add user to user dropdown
-router.post('/user', userController.createUser, (req, res) => {
+router.post('/', userController.createUser, (req, res) => {
   return res.status(200).json(res.locals.newUser);
 });
 
 // delete user from user dropdown
-router.delete('/user', userController.removeUser, (req, res) => {
-  return res.status(200).json();
+router.delete('/', userController.removeUser, (req, res) => {
+  return res.status(200).json(res.locals.removedUser);
 });
 
 // Get requests to populate all drop downs
