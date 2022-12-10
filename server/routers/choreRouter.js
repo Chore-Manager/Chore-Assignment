84b77c5fc44e6629f4f1a19859b7113f8eb25881
Rachel Cheman <rachelcheman@gmail.com>
2022-12-10 11:55:13 -0500

    add get, post, and patch routes to chore router.

--- server/routers/choreRouter.js
+++ server/routers/choreRouter.js
@@ -1,1 +1,19 @@
 // router for requests to the /chore endpoint
+const express = require('express');
+const choreController = require('../controllers/choreController');
+const router = express.Router();
+
+// get requests to chore would be for populating the chore dropdown
+router.get('/', choreController.getChores, (req, res) => {
+  return res.status(200).json(res.locals.chores);
+});
+
+// post requests for chore would be to add to the chore table in the db
+router.post('/', choreController.addChore, (req, res) => {
+  return res.status(204).json(res.locals.newChore);
+});
+
+// patch requests to chore would be for assigning new chores to a user
+router.patch('/', choreController.updateChore, (req, res) => {
+  return res.status(204).json(res.locals.updatedChore);
+});
