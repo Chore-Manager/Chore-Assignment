33e8945f0913267cba68d20fe79f78254d691968
Rachel Cheman <rachelcheman@gmail.com>
2022-12-10 11:04:40 -0500

    add initiated server pool in model. Update db structure.txt to reflect commands used to setup the db tables.

--- server/model/dbModel.js
+++ server/model/dbModel.js
@@ -1,1 +1,15 @@
-// model to setup the db pool and set the structure of the db queries
+const { Pool } = require('pg')
+
+const PG_URI =
+  'postgres://qaiuujgv:sLAXGr_6cCgMLCmZDkZKXUhKoW-Ezxns@raja.db.elephantsql.com/qaiuujgv'
+
+const pool = new Pool({
+  connectionString: PG_URI,
+})
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executing query: ', text)
+    return pool.query(text, params, callback)
+  },
+}
