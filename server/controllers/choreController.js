cf6edbcfbbd39ef198225ca7bc1ab4d722032043
Rachel Cheman <rachelcheman@gmail.com>
2022-12-10 12:30:39 -0500

    update to addChore middleware, incomplete. Update to dbStructure.txt to reflect actual names of values in db.

--- server/controllers/choreController.js
+++ server/controllers/choreController.js
@@ -4,14 +4,14 @@ const choreController = {
   // middleware to get all chores from the db
   getChores: (req, res, next) => {
     const text = `SELECT * 
-    FROM chores`;
+    FROM chores;`;
     // query the db to get all chores
     db.query(text)
       .then((data) => {
         res.locals.chores = data.rows;
         return next();
       })
-      .catche((error) => {
+      .catch((error) => {
         return next({
           log: 'error in getChores method in choreController',
           message: { error },
@@ -23,7 +23,15 @@ const choreController = {
   filterByRoom: (req, res, next) => {},
 
   // add a chore to the database
-  addChore: (req, res, next) => {},
+  addChore: (req, res, next) => {
+    const { choreName, room } = req.body;
+    const values = [choreName, room];
+    const text = `INSERT INTO chores
+    (chore, room)
+    VALUES ($1, $2);`;
+
+    db.query(text, values).then((data) => {});
+  },
 
   // assign a chore to a user
   updateChore: (req, res, next) => {},
