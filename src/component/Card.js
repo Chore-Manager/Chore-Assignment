5bf281fbf6dc3bd622054166f55d16ca8c52da7e
Tomas Kim <tk0885@gmail.com>
2022-12-10 17:28:49 -0500

    adding delete and edit data functionality, and npm packages for icons

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,21 +1,102 @@
 import React from 'react';
+import Icon from '@mdi/react';
+import { mdiDeleteForeverOutline, mdiCogOutline } from '@mdi/js';
+import style from './css/card.css';
 
-const Card = ({ personDetail }) => {
-  const { name, room, chores } = personDetail;
+const Card = ({ person }) => {
+  // const { name, room, chores } = personDetail;
+
+  //delete data from database
+  const deleteData = async () => {
+    const response = await fetch('/user', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: person.name }),
+    });
+    const data = await response.json();
+    console.log('Delete Data:', data);
+  };
+
+  //delete data from database
+  const editData = async () => {
+    const response = await fetch('/user', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: person.name }),
+    });
+    const data = await response.json();
+    console.log('Edited Data:', data);
+  };
+
+  //click on delete button
+  const onDelete = async (e) => {
+    e.preventDefault();
+    try {
+      await deleteData();
+      console.log('Delete Data:', deleteData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  //click on edit button
+  const onEdit = async (e) => {
+    e.preventDefault();
+    try {
+      await editData();
+      console.log('Edited Data:', editData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
   return (
     <div className="card">
       <div className="card-name">
-        <h2>Name: {name}</h2>
+        <h3>Name: {person.name}</h3>
       </div>
-      <div className="room">
-        <h3>Room: {room}</h3>
-      </div>
-      <div className="chores">
-        Chores:{' '}
-        {chores.map((chore, index) => (
+      {/* <div className="room">
+        <h3>Room: {person.room}</h3>
+      </div> */}
+      {/* <div className="chores">
+        Chores:  */}
+      {/* {person.map((chore, index) => (
           <h4 key={index}>{chore}</h4>
-        ))}
+        ))} */}
+      {/* </div> */}
+      <div className="footer">
+        <div className="actionsContainer">
+          <button
+            className="deleteBtn"
+            type="submit"
+            onClick={(e) => onDelete(e)}
+          >
+            <Icon
+              path={mdiDeleteForeverOutline}
+              // title="User Profile"
+              size={1}
+              horizontal
+              vertical
+              rotate={180}
+              color="red"
+            />
+          </button>
+          <button className="editBtn" type="submit" onClick={(e) => onEdit(e)}>
+            <Icon
+              path={mdiCogOutline}
+              // title="User Profile"
+              size={1}
+              horizontal
+              vertical
+              rotate={180}
+              color="red"
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/component/DisplayCard.js
+++ src/component/DisplayCard.js
@@ -4,6 +4,7 @@ import style from './css/displayCard.css';
 
 const DisplayCard = () => {
   const [people, setPeople] = useState([]);
+  // const [chores, setChores] = useState([]);
   // const data = [
   //   {
   //    users: [{id: 1, name: 'Tomas'}],
@@ -11,16 +12,12 @@ const DisplayCard = () => {
   //   },
   // ];
 
-  // {
-  //   chores: []
-  //   users: []
-  // }
   useEffect(() => {
-    fetch('/')
+    fetch('/user')
       .then((response) => response.json())
       .then((data) => {
         setPeople(data);
-        console.log('Success:', data);
+        console.log('User Res:', data);
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -28,13 +25,13 @@ const DisplayCard = () => {
   }, []);
 
   //iterating over fetched data, passing it to Card component and rendering it to the page
-  // const peopleElems = people.map((person, index) => {
-  //   return <Card key={index} person={person} />;
-  // });
-
-  const peopleElems = people.map((personDetail, index) => {
-    return <Card key={index} person={personDetail} />;
+  const peopleElems = people.map((person, index) => {
+    return <Card key={index} person={person} />;
   });
+
+  // const peopleElems = people.map((personDetail, index) => {
+  //   return <Card key={index} person={personDetail} />;
+  // });
 
   return <div className="cards-container">Cards: {peopleElems}</div>;
 };
