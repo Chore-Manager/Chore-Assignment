f82ae4fa2cbc8066ee0c774ae129c85390fae76a
John Donovan <jodonovan845@gmail.com>
2022-12-10 15:50:06 -0500

    this will add error handling to the userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,37 +4,56 @@ const db = require('../model/dbModel');
 const userController = {};
 
 userController.getUsers = async (req, res, next) => {
-  const sqlQuery = 'SELECT * FROM users';
-  const result = await db.query(sqlQuery);
-  console.log('this is the response after response.json: ', result.rows);
-  res.locals.users = result.rows;
-  return next();
+  try {
+    const sqlQuery = 'SELECT * FROM users';
+    const result = await db.query(sqlQuery);
+    res.locals.users = result.rows;
+    return next();
+  } catch (err) {
+    next({
+      log: `Error in userController.getUsers. Details: ${err}`,
+      message: { err: 'An error occurred in userController.getUsers' },
+    });
+  }
 };
 
 userController.createUser = async (req, res, next) => {
-  const { name } = req.body;
-  res.locals.newUser = name;
-  const sqlQuery = `
+  try {
+    const { name } = req.body;
+    res.locals.newUser = name;
+    const sqlQuery = `
     INSERT INTO users (name)
     VALUES ($1)
     `;
-  const insertArray = [name];
-  const result = await db.query(sqlQuery, insertArray);
-  console.log('this is the response: ', result);
-  return next();
+    const insertArray = [name];
+    const result = await db.query(sqlQuery, insertArray);
+    return next();
+  } catch (err) {
+    next({
+      log: `Error in userController.createUser. Details: ${err}`,
+      message: { err: 'An error occurred in userController.createUser' },
+    });
+  }
 };
 
 userController.removeUser = async (req, res, next) => {
-  const { name } = req.body;
-  console.log(name);
-  res.locals.removedUser = name;
-  const sqlQuery = `
-  DELETE FROM users
-  WHERE name=$1
-  `;
-  const insertArray = [name];
-  const result = await db.query(sqlQuery, insertArray);
-  return next();
+  try {
+    const { name } = req.body;
+    console.log(name);
+    res.locals.removedUser = name;
+    const sqlQuery = `
+    DELETE FROM users
+    WHERE name=$1
+    `;
+    const insertArray = [name];
+    const result = await db.query(sqlQuery, insertArray);
+    return next();
+  } catch (err) {
+    next({
+      log: `Error in userController.removeUser. Details: ${err}`,
+      message: { err: 'An error occurred in userController.removeUser' },
+    });
+  }
 };
 
 module.exports = userController;
